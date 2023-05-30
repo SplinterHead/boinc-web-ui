b87bf521c5bbdf381ed920683a2a605e0da6c563
Lewis England <lewis2004@hotmail.com>
2023-05-30 15:21:41 +0100

    tests: refactor tests to group by type

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,4 +1,4 @@
-import { createLocalVue, mount, shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import { BootstrapVue } from "bootstrap-vue";
 
 import App from "@/App";
@@ -10,21 +10,11 @@ const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
 describe("App.vue", () => {
-
   describe("a menu button", () => {
     it("is rendered", async () => {
       const wrapper = shallowMount(App, { localVue });
       const menuBtn = wrapper.get("#menu-btn");
       expect(menuBtn.isVisible()).toBe(true);
     });
-
-    // it("opens the navigation menu", () => {
-    //   const wrapper = mount(App, { localVue, attachTo: document.body });
-    //   const navBar = wrapper.getComponent("#navigation-side-bar");
-    //   expect(navBar.isVisible()).toBe(false);
-
-    //   wrapper.get("#menu-btn").trigger("click");
-    //   expect(navBar.isVisible()).toBe(true);
-    // });
   });
 });
