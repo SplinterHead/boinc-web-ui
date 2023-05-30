4d2af7411e29c5a1159d1bb3469bfb22682e2fa3
Lewis England <lewis2004@hotmail.com>
2023-05-30 15:22:42 +0100

    tests: refactor tests to group by type

--- tests/unit/NavigationBar.spec.js
+++ tests/unit/NavigationBar.spec.js
@@ -9,16 +9,18 @@ const localVue = createLocalVue();
 // install plugins as normal
 localVue.use(BootstrapVue);
 
-describe("NavigationBar renders", () => {
+describe("NavigationBar.vue", () => {
   const wrapper = shallowMount(NavigationBar, { localVue });
   const sidebar = wrapper.findComponent(BSidebar);
 
-  it("has a title of 'BOINC Client'", () => {
-    expect(sidebar.attributes("title")).toBe("BOINC Client");
-  });
+  describe("renders with the expected elements", () => {
+    it("has a title of 'BOINC Client'", () => {
+      expect(sidebar.attributes("title")).toBe("BOINC Client");
+    });
 
-  it("has a button for adding a new client", () => {
-    const newClientButton = sidebar.getComponent(BButton);
-    expect(newClientButton.text()).toBe("Add New Client...");
+    it("has a button for adding a new client", () => {
+      const newClientButton = sidebar.getComponent(BButton);
+      expect(newClientButton.text()).toBe("Add New Client...");
+    });
   });
 });
